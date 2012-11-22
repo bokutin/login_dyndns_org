2d1b213974daf265f8a5919cf9b6a6a5ec8be29f
Tomohiro Hosaka <bokutin@bokut.in>
2012-11-22 14:44:37 +0900

    warnを抑制。
    There are 2 forms with the named fields.  The first one was used. at ./script/login_dyndns_org.pl line 19.

--- script/login_dyndns_org.pl
+++ script/login_dyndns_org.pl
@@ -16,6 +16,12 @@ try {
     my $mech = WWW::Mechanize->new();
     $mech->agent_alias( 'Windows IE 6' );
     $mech->get( $top );
+    for ($mech->forms) {
+        if ($mech->attr('id') =~ m/^login\b/) {
+            $mech->form_id($mech->attr('id'));
+            last;
+        }
+    }
     $mech->submit_form(
         with_fields => {
             username => $CONFIG->config->{username},
